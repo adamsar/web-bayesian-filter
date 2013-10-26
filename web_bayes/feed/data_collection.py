4d6a1c71c0f33e641a1595831cb0b62d818f95a3
adamsar <adamsar@gmail.com>
2013-10-26 17:48:16 +0900

    Add all rss feeds for data collection

--- web_bayes/feed/data_collection.py
+++ web_bayes/feed/data_collection.py
@@ -1,10 +1,17 @@
 from web_bayes.exception.feed import FeedReadError
 from web_bayes.feed import data_writer
+import os.path
 import feedparser
 import nltk
 
 import logging
 log = logging.getLogger(__name__)
+
+UNINTERESTING_LABEL = 'uninteresting'
+INTERESTING_LABEL = 'interesting'
+
+UNINTERESTING_RSS = 'uninteresting_rss.txt'
+INTERESTING_RSS = 'interesting_rss.txt'
 
 def get_entries(feed_url):
     """
@@ -21,7 +28,7 @@ def get_data(entry):
     Returns sanitized, tokenizable text for an entry from an
     RSS feed
     """
-    return nltk.clean_html(entry['summary'])
+    return "%s %s" % (entry['title'], nltk.clean_html(entry['summary']))
 
 def collect_feed(label, name, url):
     """
@@ -32,3 +39,17 @@ def collect_feed(label, name, url):
     all_data = map(get_data, entries)
     path = data_writer.get_path(label, name)
     map(lambda d: data_writer.write_entry(path, d), all_data)
+
+    
+def process_all_feeds():
+    def process_one(label, file_name):
+        handler = open(os.path.join(data_writer.DATA_ROOT, file_name), 'r')
+        feeds = filter(lambda line: ',' in line, handler.read().split("\n"))
+        for feed in feeds:
+            rss, fname = feed.split(",")
+            collect_feed(label, fname, rss)
+    map(lambda l: process_one(l[0], l[1]), [(UNINTERESTING_LABEL,
+                                             UNINTERESTING_RSS),
+                                            (INTERESTING_LABEL,
+                                             INTERESTING_RSS)])
+    
